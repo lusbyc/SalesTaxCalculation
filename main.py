1bcea3ed5e8c6ef7c279661cb05ffc5c8f4e53da
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-05 10:35:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ print("Exercise 2: Sales Tax")
 #Refactoring may be needed for lines 5-6 and 9-10. Unsure why I'm unable to do the calculations and formatting at the same time without getting an error.
 
 price = float(input("Please enter the price of your product before tax? "))
-priceFormatted = format(price,".2f") 
-salesTax = int(input("What is the sales tax? "))
-decimalSalesTax = salesTax * .01
-salesTax_in_Dollars = price * decimalSalesTax
-salesTax_in_Dollars_Formatted = format(salesTax_in_Dollars,".2f")
-totalCost = format(price + salesTax_in_Dollars,".2f")
+price_formatted = format(price,".2f") 
+sales_tax = int(input("What is the sales tax? "))
+decimal_sales_tax = sales_tax * .01
+sales_tax_in_dollars = price * decimal_sales_tax
+sales_tax_in_dollars_formatted = format(sales_tax_in_dollars,".2f")
+total_cost = format(price + sales_tax_in_dollars,".2f")
 
 print(f"""
-Price: ${priceFormatted}
-Sales Tax: {salesTax}%
-Sales Tax in Dollars: ${salesTax_in_Dollars_Formatted}
-Total Cost: ${totalCost}""")
+Price: ${price_formatted}
+Sales Tax: {sales_tax}%
+Sales Tax in Dollars: ${sales_tax_in_dollars_formatted}
+Total Cost: ${total_cost}""")
